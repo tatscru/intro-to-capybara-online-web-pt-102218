7bf71c092c63fe804680d183ccb9d1b7b800811b
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 02:02:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
   # Write your code here!
-
+   get '/' do
+    erb :index
+    #above says to render the HTML (deliver to user's browser)
+  end
+  
+  post '/greet' do
+    erb :greet
+  end
+  # New route to respond to the form submission- what they type 
 end
--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -7,7 +7,6 @@ describe "GET '/' - Greeting Form" do
     expect(page.body).to include("Welcome!")
   end
 
-  # New test
   it 'has a greeting form with a user_name field' do
     visit '/'
 
